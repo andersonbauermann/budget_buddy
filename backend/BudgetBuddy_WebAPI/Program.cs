4b997bab944070885cc694a78703887c2fe9c0d4
Anderson Bauermann Feltes <anderson.bauermannfeltes@gmail.com>
2025-05-01 16:49:01 -0300

    feat: implement Serilog and ExceptionFilter

--- backend/BudgetBuddy_WebAPI/Program.cs
+++ backend/BudgetBuddy_WebAPI/Program.cs
@@ -2,7 +2,10 @@ using BudgetBuddy_WebAPI.Application.Interfaces;
 using BudgetBuddy_WebAPI.Application.Services.Base;
 using BudgetBuddy_WebAPI.Infrastructure.Context;
 using BudgetBuddy_WebAPI.Infrastructure.Repositories;
+using BudgetBuddy_WebAPI.WebApi.Filters;
+using BudgetBuddy_WebAPI.WebApi.Middlewares;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
 
 internal class Program
 {
@@ -12,11 +15,21 @@ internal class Program
 
         var configuration = builder.Configuration;
         var connectionString = configuration.GetConnectionString("DefaultConnection");
+        string dbBath = connectionString.Replace("Data Source=", "").TrimEnd(';');
+        var outputTemplate = "{Timestamp:dd-MM-yyyy HH:mm:ss} [{Level:u3}] {Message:lj}{NewLine}{Exception}";
+
+        Log.Logger = new LoggerConfiguration()
+            .Enrich.FromLogContext()
+            .WriteTo.SQLite(dbBath, tableName: "Logs", batchSize: 1, restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Warning)
+            .WriteTo.Console(outputTemplate: outputTemplate, theme: Serilog.Sinks.SystemConsole.Themes.AnsiConsoleTheme.Code)
+            .CreateLogger();
+
+        builder.Logging.ClearProviders();
+        builder.Host.UseSerilog();
 
         builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite(connectionString));
 
         builder.Services.AddControllers();
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
 
@@ -26,27 +39,28 @@ internal class Program
         builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
         builder.Services.AddScoped<IExpenseInstallmentRepository, ExpenseInstallmenRepository>();
 
-        //builder.Services.AddScoped<CreateExpenseService>();
+        builder.Services.AddControllers(options =>
+        {
+            options.Filters.Add<ExceptionFilter>();
+        });
 
         var app = builder.Build();
 
-        // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
         {
             app.UseSwagger();
             app.UseSwaggerUI();
         }
 
+        app.UseMiddleware<LoggingMiddleware>();
         app.UseHttpsRedirection();
-
         app.UseAuthorization();
-
         app.UseCors(option => option.AllowAnyHeader()
                                     .AllowAnyMethod()
                                     .AllowAnyOrigin());
 
         app.MapControllers();
-
         app.Run();
+        Log.CloseAndFlush();
     }
 }
---
+++ backend/BudgetBuddy_WebAPI/WebApi/Middlewares/LoggingMiddleware.cs
@@ -0,0 +1,21 @@
+﻿using Serilog.Context;
+
+namespace BudgetBuddy_WebAPI.WebApi.Middlewares;
+
+public class LoggingMiddleware(RequestDelegate next)
+{
+    private readonly RequestDelegate _next = next;
+
+    public async Task Invoke(HttpContext context, ILogger<LoggingMiddleware> logger)
+    {
+        var route = context.Request.Path;
+        var method = context.Request.Method;
+
+        using (LogContext.PushProperty("Route", route))
+        using (LogContext.PushProperty("Method", method))
+        {
+            logger.LogInformation("Requisição recebida: {Method} {Route}");
+            await _next(context);
+        }
+    }
+}
